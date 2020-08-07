31e3f674bfb043117f694e056b9b9153246f2244
Ben Barbersmith <ben@barbersmith.com>
2020-08-07 23:00:13 +0100

    Add mini.css stylesheet and restyle nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,67 +3,29 @@
 </script>
 
 <style>
-  nav {
-    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-    font-weight: 300;
-    padding: 0 1em;
-  }
-
-  ul {
-    margin: 0;
-    padding: 0;
-  }
-
-  /* clearfix */
-  ul::after {
-    content: "";
-    display: block;
-    clear: both;
-  }
-
-  li {
-    display: block;
-    float: left;
-  }
-
   [aria-current] {
-    position: relative;
-    display: inline-block;
-  }
-
-  [aria-current]::after {
-    position: absolute;
-    content: "";
-    width: calc(100% - 1em);
-    height: 2px;
-    background-color: rgb(0, 0, 0);
-    display: block;
-    bottom: -1px;
-  }
-
-  a {
-    text-decoration: none;
-    padding: 1em 0.5em;
-    display: block;
+    font-weight: bolder;
   }
 </style>
 
-<nav>
-  <ul>
-    <li>
-      <a aria-current={segment === undefined ? 'page' : undefined} href=".">
-        home
-      </a>
-    </li>
-    <li>
-      <a aria-current={segment === 'config' ? 'page' : undefined} href="config">
-        config
-      </a>
-    </li>
-    <li>
-      <a aria-current={segment === 'about' ? 'page' : undefined} href="about">
-        about
-      </a>
-    </li>
-  </ul>
-</nav>
+<header>
+  <a href="#/" class="logo">LFG</a>
+  <a
+    aria-current={segment === undefined ? 'page' : undefined}
+    href="."
+    class="button">
+    friends
+  </a>
+  <a
+    aria-current={segment === 'config' ? 'page' : undefined}
+    href="config"
+    class="button">
+    config
+  </a>
+  <a
+    aria-current={segment === 'about' ? 'page' : undefined}
+    href="about"
+    class="button">
+    about
+  </a>
+</header>
