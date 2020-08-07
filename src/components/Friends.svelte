0779ee3c9e8b92c1ffb13a60ee559587c1cb23ce
Ben Barbersmith <ben@barbersmith.com>
2020-08-07 23:12:28 +0100

    Improve styling for friends list

--- src/components/Friends.svelte
+++ src/components/Friends.svelte
@@ -3,21 +3,42 @@
 </script>
 
 <div id="friends">
-  {#if typeof friends !== 'undefined' && friends.length > 0}
+  {#if typeof friends == 'undefined'}
+    <p>
+      No friends found.
+      <a href="/config">Configure</a>
+      your Steam Web API key.
+    </p>
+  {:else if friends.length > 0}
     <h2>Games in common</h2>
-    {#each friends as friend}
-      <h3>{friend.name}</h3>
-      {#if friend.games.length > 0}
-        <ul>
-          {#each friend.games as game}
-            <li>{game.name}</li>
-          {/each}
-        </ul>
-      {:else}
-        <p>No matching games.</p>
-      {/if}
-    {/each}
+    <div class="collapse">
+      {#each friends as friend, i}
+        <input
+          type="checkbox"
+          id="collapse-section{i}"
+          checked
+          aria-hidden="true" />
+        <label for="collapse-section{i}" aria-hidden="true">
+          {friend.name}
+        </label>
+        <div>
+          {#if friend.games.length > 0}
+            <ul>
+              {#each friend.games as game}
+                <li>{game.name}</li>
+              {/each}
+            </ul>
+          {:else}
+            <p>No matching games.</p>
+          {/if}
+        </div>
+      {/each}
+    </div>
   {:else}
-    <p>No friends found.</p>
+    <p>
+      No friends found. If you're sure you have some,
+      <a href="/config">check you've configured</a>
+      your Steam Web API key.
+    </p>
   {/if}
 </div>
